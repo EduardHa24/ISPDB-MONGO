e414d45464e76c9f41d936b07e4b892ab638cb24
EduardHa24 <eddiehaimov25@gmail.com>
2024-02-03 15:43:11 +0200

    Added count to check if exists

--- ISPDB_Project.py
+++ ISPDB_Project.py
@@ -5,19 +5,19 @@ client = MongoClient("mongodb://localhost:27017")
 db = client.ISPDB
 collection = db.Devices
 
-Device_Name = input("Enter name here: ")
-Device_Ver = input("Enter Version: ")
-Device_Model = input("Enter Model name: ")
+Device_Name = input("Enter name here: ").strip()
+Device_Ver = input("Enter Version: ").strip()
+Device_Model = input("Enter Model name: ").strip()
 
-for col in collection.find({'name':Device_Name}):
-    if col != Device_Name:
-        print("Device already exists")
-    else:
-        collection.insert_one({"name": Device_Name,
-                                "model": Device_Model,
-                                "version":Device_Ver,})
-        print("Device added")
-    continue
+exists = collection.count_documents({'name':Device_Name})
+
+if exists > 0:
+    print("Device already exists")
+else:
+    collection.insert_one({"name": Device_Name,
+                            "model": Device_Model,
+                            "version":Device_Ver,})
+    print("Device added")
 
 for x in collection.find():
      print(x)
