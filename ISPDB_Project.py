f51b3a08084478185c2fb294ac219f33f4eb3fe4
EduardHa24 <eddiehaimov25@gmail.com>
2024-02-03 15:09:03 +0200

    Addeding For loop

--- ISPDB_Project.py
+++ ISPDB_Project.py
@@ -3,21 +3,21 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017")
 
 db = client.ISPDB
-col = db.Devices
+collection = db.Devices
 
 Device_Name = input("Enter name here: ")
 Device_Ver = input("Enter Version: ")
 Device_Model = input("Enter Model name: ")
 
+for col in collection.find({'name':Device_Name}):
+    if col != Device_Name:
+        print("Device already exists")
+    else:
+        collection.insert_one({"name": Device_Name,
+                                "model": Device_Model,
+                                "version":Device_Ver,})
+        print("Device added")
+    continue
 
-if Device_Name == Device_Model:
-        print("Device Already Exists")
-else:
-        col.insert_one({"name": Device_Name,
-                        "model": Device_Model,
-                        "version":Device_Ver,})
-
-        print("Data Added")
-
-for col in col.find():
-    print(col)
+for x in collection.find():
+     print(x)
