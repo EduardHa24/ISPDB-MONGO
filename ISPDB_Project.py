44ee977ef5d5bdf51c2c6768e709ab7ef6929965
EduardHa24 <eddiehaimov25@gmail.com>
2024-02-03 13:55:47 +0200

    Second Commit

--- ISPDB_Project.py
+++ ISPDB_Project.py
@@ -10,21 +10,14 @@ Device_Ver = input("Enter Version: ")
 Device_Model = input("Enter Model name: ")
 
 
-def insert_data(Device_Name,Device_Ver,Device_Model):
-    if Device_Name == Device_Name:
+if Device_Name == Device_Model:
         print("Device Already Exists")
-    else:
+else:
         col.insert_one({"name": Device_Name,
                         "model": Device_Model,
                         "version":Device_Ver,})
 
         print("Data Added")
-    return insert_data(Device_Name,Device_Ver,Device_Model)
-
-
-
-    
-
 
 for col in col.find():
     print(col)
